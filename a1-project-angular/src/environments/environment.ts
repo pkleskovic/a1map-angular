b29ca6912b6a554f24da3850cfdb2c6fd822de43
pkleskovic <pxk9023@rit.edu>
2020-04-15 17:21:24 +0200

    Fixed firebase config

--- a1-project-angular/src/environments/environment.ts
+++ a1-project-angular/src/environments/environment.ts
@@ -5,12 +5,14 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: 'AIzaSyDkYMl6M4WU2FLqdqLC25zK9jvGvQM37n0',
-    authDomain: 'YOUR_AUTH_DOMAIN',
-    databaseURL: 'YOUR_DATABASE_URL',
-    projectId: 'a1-smart-city-app',
-    storageBucket: 'YOUR_STORAGE_BUCKET',
-    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID'
+    apiKey: 'AIzaSyAbMnpIBztmAGztNg9IBJc_zncGsED5fBw',
+    authDomain: 'a1-smart-city-map.firebaseapp.com',
+    databaseURL: 'https://a1-smart-city-map.firebaseio.com',
+    projectId: 'a1-smart-city-map',
+    storageBucket: 'a1-smart-city-map.appspot.com',
+    messagingSenderId: '882371270107',
+    appId: '1:882371270107:web:cc7764541f6d3cf11a83fe',
+    measurementId: 'G-KTLQRG6GF9'
   }
 };
 
