5ad31912cd0318d7bcfcccde414318912be8cdb6
pkleskovic <pxk9023@rit.edu>
2020-04-17 08:33:37 +0200

    rest of the commit because i always forget to navigate to project root

--- a1-project-angular/src/app/models/bill.model.ts
+++ a1-project-angular/src/app/models/bill.model.ts
@@ -3,7 +3,19 @@ import { Product } from './product.model';
 export class Bill {
   id: string;
   date: string;
-  userId: number;
+  userId: string;
   products: Array<Product>;
   total: number;
+
+  constructor(
+    date: string,
+    userId: string,
+    products: Array<Product>,
+    total: number
+  ){
+    this.date = date;
+    this.userId = userId;
+    this.products = products;
+    this.total = total;
+  }
 }
